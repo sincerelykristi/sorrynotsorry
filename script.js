736c66cbf44315c007cc617b87b7602acdbf8d52
Kristi Murphy <kristilmurphy@gmail.com>
2016-11-02 16:57:19 -0400

    commit #2 for Wednesday

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 jQuery(function() {
 
 
-// var $peg1 = $("#peg1");
-// var $space7 = $(".7")
+var $instructsection = $("#instructions");
+var $gamesection = $("#game");
+var $aboutsection = $("#about");
+
+console.log($gamesection);
+console.log($instructsection);
+console.log($aboutsection);
+
+// navigation button functions
+// Instructions Button
+function clickInstructions() {
+  $("#ins").click(function(){
+    console.log("instructions clicked!")
+    $gamesection.css('visibility', 'hidden');
+    $aboutsection.css('visibility', 'hidden');
+    $instructsection.css('visibility', 'visible');
+  });
+};
+
+clickInstructions();
+
+// Game Button
+function clickGame() {
+  $("#ga").click(function(){
+    console.log("game clicked!")
+    $instructsection.css('visibility', 'hidden');
+    $aboutsection.css('visibility', 'hidden');
+    $gamesection.css('visibility', 'visible');
+  });
+};
+
+clickGame();
+
+// About Button
+function clickAbout() {
+  $("#ab").click(function(){
+    console.log("about clicked!")
+    $instructsection.css('visibility', 'hidden');
+    $gamesection.css('visibility', 'hidden');
+    $aboutsection.css('visibility', 'visible');
+  });
+};
+
+clickAbout();
 
 
-// var movePeg = function() {
-//   ($space7).append($peg1);
-// };
 
-// movePeg();
+
 
 
 
